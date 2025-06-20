be3da7c56056ad1b9d6bbd7828a63590ccb8bbb4
sovan-souern <sovan.souern@student.passerellesnumeriques.org>
2025-06-20 18:49:55 +0700

    push code

---
+++ database/migrations/2025_06_20_085952_create_borrows_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('borrows', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('stared_at')->nullable();
+            $table->timestamp('end_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('borrows');
+    }
+};
